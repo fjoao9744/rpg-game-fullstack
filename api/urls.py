d3cb2c69561c82dbd2de00ca8ab996986b8d4ef1
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-04-20 17:12:47 -0300

    api test

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from api import views
 
+from main import views as v # test
+
 urlpatterns = [
     path("", views.Players.as_view()),
+    path("test/", v.api_test) # test
 ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 def main(request):
 
-
-
     return render(request, "main.html")
+
+def api_test(request): # test
+    return render(request, "api.html")
 
 
