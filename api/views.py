7725d2bd055300c06cd6a893f756678ae52e1d5c
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-04-21 18:58:54 -0300

    delete e patch

--- api/views.py
+++ api/views.py
@@ -46,16 +46,39 @@ class Players(APIView):
         except Player.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         
-        serializer = PlayerSerializers(player, data=request.data)
+        serializer = PlayerSerializers(player, data=request.data, partial=False)
         
         if serializer.is_valid():
             serializer.save()
+            
             return Response(serializer.data, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-    def patch(self):
-        ...
+    def patch(self, request):
+        player_id = request.data.get("id")
+        try:
+            player = Player.objects.get(id=player_id)
+            
+        except Player.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         
-    def delete(self):
-        ...    
+        serializer = PlayerSerializers(player, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request):
+        name = request.query_params.get("name")
+        try:
+            player = Player.objects.get(name=name)
+            player.delete()
+            
+            return Response(status=status.HTTP_204_NO_CONTENT)  # Retorna sucesso
+        
+        except Player.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
