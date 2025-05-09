883fdb3d5fac8c0e4a18a393d99d9a1b881ef296
dyxm9 <154237716+fjoao9744@users.noreply.github.com>
2025-05-09 11:28:25 -0300

    collectstatic

---
+++ staticfiles/js/script.js
@@ -0,0 +1,39 @@
+document.body.onload = async () => {
+    let player = localStorage.getItem("player");
+
+    let logged = Boolean(localStorage.getItem("logged"))
+    console.log(logged)
+    
+    if (logged) {
+        logar(false)
+    } else {
+        player = await fetch("static/data/player.json") // player vazio
+        .then(response => response.json())
+
+    }
+    
+    const monsters_gen = monsters() // gerador de monstros
+
+    let monster = await monsters_gen.next() // toda vez que chamar o .next() ele vai dar o proximo monstro
+
+    console.log(monster.value)
+
+    monster = await monsters_gen.next()
+    console.log(monster.value)
+
+    monster = await monsters_gen.next()
+    console.log(monster.value)
+
+}
+
+async function* monsters() { // gerador que retorna monstro por monstro
+    const monsters = await fetch("static/data/monsters.json").then(response => response.json())
+    for (let monster of Object.values(monsters)) {
+        yield monster
+        
+    }
+
+}
+
+
+
