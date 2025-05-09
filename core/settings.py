b8a986690f8bd0b263e57d3ecbf3128c97bd4c62
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-05-08 21:01:33 -0300

    .env

--- core/settings.py
+++ core/settings.py
@@ -67,12 +67,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DEBUG = True
+
+import dj_database_url
+from decouple import config
+import os
+
+IS_PRODUCTION = config('IS_PRODUCTION', default=False, cast=bool)
+
+if IS_PRODUCTION:
+    DATABASES = {
+        'default': dj_database_url.parse(config("DATABASE_URL"))
     }
-}
+    
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
