08a2e513a959a93ac98a4c9922c21bf56427e685
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-05-26 06:22:30 -0300

    media

--- battle/views.py
+++ battle/views.py
@@ -14,16 +14,22 @@ def choice_monster(request, floor_num):
                 
     # pegar os monstros do andar que o user passou
     floor_list = floors.items()
-    floor = tuple(filter(lambda floor: int(floor[0].split()[-1]) == floor_num, floor_list))[0]    
+    floor = tuple(filter(lambda floor: int(floor[0].split()[-1]) == floor_num, floor_list))[0]
+    print(floor[1].get("type"))
     
-    monsters = floor[1].get("monsters")    
+    if floor[1].get("type") == "battle":
+        monsters = floor[1].get("monsters")    
+        
+        # sortear os monstros
+        weights = [list(monster.values())[0] / 100 for monster in monsters]   
+        print(weights) 
+        
+        monster = random.choices(monsters, weights=weights, k=1)[0]
+        
+        return get_status_monster(request, monster)
     
-    # sortear os monstros
-    weights = [list(monster.values())[0] / 100 for monster in monsters]    
-    
-    monster = random.choices(monsters, weights=weights, k=1)[0]
-    
-    return get_status_monster(request, monster)
+    else:
+        return JsonResponse({"message": "is a shop"}, safe=False)
     
 def get_status_monster(request, monster):
     file_path = os.path.join(settings.BASE_DIR, 'static', 'data', 'monsters.json')
