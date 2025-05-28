339daf85c783a90245895c2bac0e9110c6e7084f
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-05-28 16:36:24 -0300

    Update urls.py

--- game/player/urls.py
+++ game/player/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from game.player.views import PlayerView,Floor, NextFloor, PastFloor
+from game.player.views import PlayerView,Floor, NextFloor, PastFloor, 
 
 urlpatterns = [
-    path("<str:player_name>", PlayerView.as_view()),
-    path("floor/<str:player_name>", Floor.as_view()),
-    path("floor/next/<str:player_name>", NextFloor.as_view()),
-    path("floor/past/<str:player_name>", PastFloor.as_view()),
+    # andares
+    path("floor/next/<str:player_name>/", NextFloor.as_view()),
+    path("floor/past/<str:player_name>/", PastFloor.as_view()),
+    path("floor/<str:player_name>/", Floor.as_view()),
+    
+    # attacks
+    path("attack/<str:player_name/<int:attack_num>/", PlayerAttackView.as_view()),
+    path("attack/<str:player_name/", PlayerAttackView.as_view()),
+
+    # player
+    path("<str:player_name>/", PlayerView.as_view()),
 
 
 ]
