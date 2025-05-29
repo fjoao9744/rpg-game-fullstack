81be28d5c61d2ed7d1419fcfb0ac467acf0015e4
dyxm9 <154237716+fjoao9744@users.noreply.github.com>
2025-05-29 11:58:15 -0300

    atualização sidebar

--- static/js/utils.js
+++ static/js/utils.js
@@ -3,10 +3,35 @@ async function sendLog(message, id="", classe="") {
     messageTag.innerHTML = message;
     messageTag.id = id;
     messageTag.className = classe;
-    document.getElementById("chat__div").appendChild(messageTag);
+    document.querySelector(".text-sidebar__area").appendChild(messageTag);
 
     return messageTag;
 }
+
+let isOpen = false;
+document.getElementById("toggleSidebar").onclick = () => {
+  const sidebar = document.getElementById("chat__div");
+
+  if (isOpen) {
+    sidebar.classList.remove("sidebar__open");
+  } else {
+    sidebar.classList.add("sidebar__open");
+  }
+  isOpen = !isOpen;
+  }
+  
+  document.getElementById("close-sidebar").onclick = () => {
+    const sidebar = document.getElementById("chat__div");
+
+  if (isOpen) {
+    sidebar.classList.remove("sidebar__open");
+  } else {
+    sidebar.classList.add("sidebar__open");
+  }
+
+  isOpen = !isOpen;
+  }
+
 
 async function sendButton(message, func=null, id="", classe="") {
     let buttonTag = document.createElement("button");
